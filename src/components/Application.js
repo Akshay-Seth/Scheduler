f19622115a5f86a7b961b3aba9680a8b31fd8a2a
BeamMeup-fail <51726921+BeamMeup-fail@users.noreply.github.com>
2021-08-02 14:25:01 -0700

    ubated apps

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,11 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        {<img
+  className="sidebar--centered"
+  src="images/logo.png"
+  alt="Interview Scheduler"
+/>}
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
