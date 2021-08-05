2a2ec06bd301c0d35e77e30973343f1c1b41e6f9
BeamMeup-fail <51726921+BeamMeup-fail@users.noreply.github.com>
2021-08-04 20:05:45 -0700

    added selector and updated app to take in urls from axio

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,52 @@
 import React, { useState } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
+import { getAppointmentsForDay } from "helpers/selectors";
+import { getInterview } from "helpers/selectors";
+import { getInterviewersForDay } from "helpers/selectors";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+const [days, setDays] = useState([]);
+useEffect(() => {
+  Promise.all([
+    Promise.resolve(axios.get(`http://localhost:8001/api/days`)),
+    Promise.resolve(axios.get(`http://localhost:8001/api/appointments`)),
+    Promise.resolve(axios.get(`http://localhost:8001/api/interviewers`))
+  ]).then(all => {
+    const [days, appointments, interviewers] = all;
+    dispatch({
+      type: SET_APPLICATION_DATA,
+      days: days.data,
+      appointments: appointments.data,
+      interviewers: interviewers.data
+    });
+  });
+}, []);
 
-
-
+return { state, setDay, bookInterview, cancelInterview };
+}
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
+
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const appointmentList = appointments.map( appointment => {
+    return(
+      <Appointment 
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={getInterview(state, appointment.interview)}
+        interviewers={getInterviewersForDay(state, state.day)}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
---
+++ src/components/helpers/selectors.js
@@ -0,0 +1,60 @@
+export function getAppointmentsForDay(state, day) {
+    const apptmt = [];
+  
+    for (let dayId of state.days) {
+      if (dayId.name === day) {
+        const appointments = dayId.appointments;
+        for (let apptmtID of appointments) {
+          for (const aId in state.appointments) {
+            if (Number(aId) === Number(apptmtID)) {
+              apptmt.push(state.appointments[aId]);
+            }
+          }
+        }
+      }
+    }
+  
+    return apptmt;
+  }
+  
+  export function getInterviewersForDay(state, day) {
+    const ivw = [];
+  
+    for (let dayId of state.days) {
+      if (dayId.name === day) {
+        const interviewers = dayId.interviewers;
+        for (let ivwID of interviewers) {
+          for (const iId in state.interviewers) {
+            if (Number(iId) === Number(ivwID)) {
+              ivw.push(state.interviewers[iId]);
+            }
+          }
+        }
+      }
+    }
+  
+    return ivw;
+  }
+  
+  export function getInterview(state, appointment) {
+    let interviewData = {};
+    if (appointment !== null) {
+      const interNum = appointment["interviewer"];
+      for (const iId in state.interviewers) {
+        if (Number(iId) === Number(interNum)) {
+          interviewData.student = appointment["student"];
+          interviewData.interviewer = {};
+          interviewData.interviewer.id = state.interviewers[iId].id;
+          interviewData.interviewer.name = state.interviewers[iId].name;
+          interviewData.interviewer.avatar = state.interviewers[iId].avatar;
+        }
+      }
+    }
+    if (Object.keys(interviewData).length === 0) {
+      return null;
+    } else {
+      return interviewData;
+    }
+  }
+
+  module.exports{}
