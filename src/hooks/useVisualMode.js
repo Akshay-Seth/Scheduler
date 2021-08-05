6d719a23c3537fbc7238fbf342b6c5e9891791c2
BeamMeup-fail <51726921+BeamMeup-fail@users.noreply.github.com>
2021-08-05 11:40:51 -0700

    Added visual mode and updated the appointmentuse funtion

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(next, replace) {
+    history.push(next);
+    setMode(next);
+    if (replace) {
+      history.pop();
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    } else if (history.length === 1) {
+      setMode(initial);
+    }
+  }
+
+  return { mode, transition, back };
+}
